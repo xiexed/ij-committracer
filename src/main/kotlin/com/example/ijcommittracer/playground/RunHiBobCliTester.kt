494b8a9510f8d5b220655712a5d307476cdb68cf
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2025-03-20 01:23:29 +0300

    Add HiBob command-line testing tools and documentation

---
+++ src/main/kotlin/com/example/ijcommittracer/playground/RunHiBobCliTester.kt
@@ -0,0 +1,13 @@
+package com.example.ijcommittracer.playground
+
+/**
+ * Main class for running the HiBob CLI tester.
+ * This is a simple entry point to make it easier to run the CLI tool.
+ */
+object RunHiBobCliTester {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Launching HiBob CLI Tester...")
+        HiBobCliTester.main(args)
+    }
+}
